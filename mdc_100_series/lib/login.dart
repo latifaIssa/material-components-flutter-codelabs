eee62b501bb147d1a3b57c0548f6c8cd547fbb6a
latifaIssa <latifa.masri1998@gmail.com>
2021-02-23 18:27:15 +0200

    login page

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -21,6 +21,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,11 +39,47 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             SizedBox(height: 120.0),
-            // TODO: Wrap Username with AccentColorOverride (103)
+
+            // / [Name]
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            // spacer
+            SizedBox(height: 12.0),
+            // [Password]
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
             // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
+            // TODO: Add buttons (101)
+            FlatButton(
+              child: Text('CANCEL'),
+              onPressed: () {
+                // TODO: Clear the text fields (101)
+                _usernameController.clear();
+                _passwordController.clear();
+              },
+            ),
+            // TODO: Add an elevation to NEXT (103)
+            // TODO: Add a beveled rectangular border to NEXT (103)
+            RaisedButton(
+              child: Text('NEXT'),
+              onPressed: () {
+                // TODO: Show the next page (101)
+                Navigator.pop(context);
+              },
+            ),
           ],
         ),
       ),
